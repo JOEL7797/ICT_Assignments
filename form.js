6b718411125f55ae149913b92ff7ea3c5f6ac9a7
Joel <joel7797@gmail.com>
2021-03-29 15:15:31 +0530

    First commit: First Version

---
+++ form.js
@@ -0,0 +1,34 @@
+let pwd=document.getElementById("pwd");
+let email=document.getElementById("email");
+let phone=document.getElementById("phone");
+let error=document.getElementById("error");
+
+function validate()
+
+{
+    if (email.value==""||pwd.value=="")
+    {
+        alert("Fields cannot be empty");
+        return false;
+    } 
+    else
+    {
+        let regexp=/^([A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+).([a-z{2,3}])(.[a-z]{2,3})?$/
+        if(regexp.test(email.value))
+        {
+            error.innerHTML="valid";
+            error.style.color="green";
+            return true;
+        }
+
+        else
+
+        {
+            error.innerHTML="invalid";
+            error.style.color="red";
+            return false;
+        }
+
+    }   
+}
+
